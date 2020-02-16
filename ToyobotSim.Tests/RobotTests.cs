d11f63d20a0030541c6e2151123181d41175285f
Ravi Attota <raveendrahq@gmail.com>
2020-02-16 22:10:33 +0000

    Committing missing files

---
+++ ToyobotSim.Tests/RobotTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToyRobot;
+
+namespace ToyobotSim.Tests
+{
+    [TestClass]
+    public class RobotTests
+    {
+        [TestMethod]
+        public void TestTurnLeft()
+        {
+        }
+
+        [TestMethod]
+        public void TestTurnRight()
+        {
+        }
+
+
+        [TestMethod]
+        public void TestRobotMove()
+        {
+        }
+    }
+}
